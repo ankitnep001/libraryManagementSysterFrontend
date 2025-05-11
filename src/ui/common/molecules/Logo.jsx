d7fced3ca00e3ae954e55b5a7029184c7c7fbd96
Ankit <ankit001nep@gmail.com>
2025-05-11 11:20:22 +0545

    add: logo component as functional component

--- src/ui/common/molecules/Logo.jsx
+++ src/ui/common/molecules/Logo.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { image } from '../../../config/constant/image'
+import { Link } from 'react-router-dom';
+import { image } from '../../../config/constant/image';
 
-const Logo = () => {
+const Logo = ({ height = 'h-10', width = 'w-10', className = '' }) => {
     return (
-        <Link to='/home'>
-            <img className='h-30 w-30' src={image.logo} alt="" />
+        <Link to='/home' className={`${height} ${width} ${className}`}>
+            <img
+                src={image.logo}
+                alt="Logo"
+                className={`${height} ${width} object-contain`}
+            />
         </Link>
-    )
-}
+    );
+};
 
-export default Logo
+export default Logo;
