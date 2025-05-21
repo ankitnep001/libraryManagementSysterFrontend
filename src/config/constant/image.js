3f3435f20906ac3492b6f7419e155c50088ac3fd
Ankit <ankit001nep@gmail.com>
2025-05-21 21:06:23 +0545

    edit: image & navbar

--- src/config/constant/image.js
+++ src/config/constant/image.js
@@ -1,6 +1,7 @@
+import fallback from '../../assets/images/fallback-image.png'
+import libraryHero from '../../assets/images/library.png'
 import logo from '../../assets/images/logo.png'
-
 export const image = {
 
-    logo
+    logo, libraryHero, fallback
 }
--- src/data/userMenuNavbar.js
+++ src/data/userMenuNavbar.js
@@ -1,30 +1,17 @@
-// src/data/menuItems.js
+
 export const menuItems = [
     {
         title: "Home",
         path: "/",
 
     },
-    // {
-    //     title: "Browse",
-    //     path: "/browse",
-    //     icon: "📚"
-    // },
+
     {
-        title: "Books",
-        path: "/book",
+        title: "Category",
+        path: '/category'
 
     },
-    // {
-    //     title: "Genres",
-    //     path: "/genres",
 
-    //     submenu: [
-    //         { title: "Fiction", path: "/genres/fiction" },
-    //         { title: "Non-Fiction", path: "/genres/non-fiction" },
-    //         { title: "Science", path: "/genres/science" }
-    //     ]
-    // },
     {
         title: "About",
         path: "/about",
